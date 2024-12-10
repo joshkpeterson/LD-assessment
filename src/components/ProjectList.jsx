9909c106b4a497fe9c4a3e76e5609a3767636389
Josh Peterson <joshkpete@gmail.com>
2024-12-10 20:42:52 +0100

    Try fix layout bug

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -5,27 +5,28 @@ import styles from '../styles/ProjectList.module.scss'
 
 function ProjectList({ projects }) {
 
-  const [isGrid, setIsGrid] = useState(false)
+  const [isList, setIsList] = useState(true)
 
-  const projectListStyles = `${styles.projectListContainer} ${isGrid ? 
+  const projectListStyles = `${styles.projectListContainer} ${isList ? 
     styles['projectListContainer--isList'] : ''}`
 
   return (
     <div className={styles.projectList__outerContainer}>
+
       <div className={projectListStyles}>
-        <div class={styles.projectListContainer__controls}> 
-          <button onClick={() => setIsGrid(false)} 
-            class={styles.projectListContainer__controls__listButton}>
+        <div className={styles.projectListContainer__controls}> 
+          <button onClick={() => setIsList(true)} 
+            className={styles.projectListContainer__controls__listButton}>
             List
           </button>
-          <button onClick={() => setIsGrid(true)} 
-            class={styles.projectListContainer__controls__gridButton}>
+          <button onClick={() => setIsList(false)} 
+            className={styles.projectListContainer__controls__gridButton}>
             Grid
           </button>
         </div>
         {
           projects.map(project => 
-            <ProjectCard key={project.id} project={project} isList={!isGrid}/>
+            <ProjectCard key={project.id} project={project} isList={isList}/>
           )
         }
       </div>
