304a2f55e8611705802eed6fe329d9952f4dee32
Josh Peterson <joshkpete@gmail.com>
2024-12-10 17:58:55 +0100

    Add rough styles for list layout

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,26 +2,38 @@ import React from 'react'
 import styles from '../styles/ProjectCard.module.scss'
 
 function ProjectCard({ project: { title, description, img_url, img_caption, location, tags } }) {
-  const daysLeft = 5;
-  const daysLeftLabel = `${daysLeft} days left`;  
-  const progress = 50;
-  const progressBarWidthStyle = { width: `${progress}%` };  
+  const daysLeft = 5
+  const daysLeftLabel = `${daysLeft} days left`
+  const progress = 50
+  const progressBarWidthStyle = { width: `${progress}%` }
+  
+  const isList = true;
+  const projectCardStyles = `${styles.projectCardContainer} ${isList ? 
+      styles['projectCardContainer--isList'] : ''}`
+
+// console.log(projectCardStyles)
+// console.log(styles.projectCardContainer)
+// console.log(styles['projectCardContainer--isList'])
+// // console.log(styles.projectCardContainer__isList)
 
   return (
-    <div className={styles.projectCardContainer}>
-      <img className={styles.projectCardContainer__image} src={img_url} alt="" />
-      <p className={styles.projectCardContainer__img_caption}>{img_caption}</p>
-      <p className={styles.projectCardContainer__location}>{location}</p>
-      <ul className={styles.projectCardContainer__tags}>
-        { tags.map(({ tagName }) => <li key={tagName}>{tagName}</li>)}
-      </ul>
-      <p className={styles.projectCardContainer__title}>{title}</p>
-      <p className={styles.projectCardContainer__description}>{description}</p>
-      <div className={styles.projectCardContainer__progressBar}>
-        <div style={progressBarWidthStyle}></div>
-      </div> 
-      <p className={styles.projectCardContainer__daysLeftLabel}>{daysLeftLabel}</p>
-
+    <div className={projectCardStyles}>
+      <div>
+        <img className={styles.projectCardContainer__image} src={img_url} alt="" />
+        <p className={styles.projectCardContainer__img_caption}>{img_caption}</p>
+      </div>
+      <div>
+        <p className={styles.projectCardContainer__location}>{location}</p>
+        <ul className={styles.projectCardContainer__tags}>
+          { tags.map(({ tagName }) => <li key={tagName}>{tagName}</li>)}
+        </ul>
+        <p className={styles.projectCardContainer__title}>{title}</p>
+        <p className={styles.projectCardContainer__description}>{description}</p>
+        <div className={styles.projectCardContainer__progressBar}>
+          <div style={progressBarWidthStyle}></div>
+        </div> 
+        <p className={styles.projectCardContainer__daysLeftLabel}>{daysLeftLabel}</p>
+      </div>
     </div>
   )
 }
--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -4,16 +4,21 @@ import styles from '../styles/ProjectList.module.scss'
 
 
 function ProjectList({ projects }) {
-  return (
-    <div className={styles.outerContainer}>
+  const isList = true;
 
-    <div className={styles.projectListContainer}>
-    { 
-      projects.map(project => 
-        <ProjectCard key={project.id} project={project} />
-      )
-    }
-    </div>
+  const projectListStyles = `${styles.projectListContainer} ${isList ? 
+    styles['projectListContainer--isList'] : ''}`
+
+  return (
+    <div className={styles.projectList__OuterContainer}>
+
+      <div className={projectListStyles}>
+      { 
+        projects.map(project => 
+          <ProjectCard key={project.id} project={project} />
+        )
+      }
+      </div>
 
     </div>
 
